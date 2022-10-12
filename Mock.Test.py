bb662eabcfe2e161ac767deb59e4875cb2b51ef3
apatel5906 <100331291+apatel5906@users.noreply.github.com>
2022-10-12 18:50:54 -0400

    Update Mock.Test.py

--- Mock.Test.py
+++ Mock.Test.py
@@ -1,52 +1,33 @@
 """
-Arpit Patel 
+
 
 
 """
-from githubAPI import get_repos, retrieve_commits
+from GitApi import get_repos, retrieve_commits
 from unittest import mock
 import unittest
 
 
-class MockGitAPI(unittest.TestCase):
-
-    @mock.patch('GitApi.get_repos')
-    def mock_get_repos_1(self, mock_repo_names):
-        mock_repo_names.return_value = ['apatel5906','exp1-learning','SSW--567','HW-02b' , 'HW-04a']
-
-        repos = get_repos('apatel5906')
-        self.assertGreaterEqual(len(repos), 5, "User 'apatel5906' has 5 repositories")
-        self.assertIn('apatel5906', repos)
-        self.assertIn('SSW--567', repos)
-        self.assertIn('HW-04a', repos)
-        self.assertIn('HW-02b', repos)
-        self.assertIn('exp1-learning', repos)
+class MockGitApi(unittest.TestCase):
 
     @mock.patch('GitApi.get_repos')
     def mock_get_repos_2(self, mock_repo_names):
-        mock_repo_names.return_value = ['apatel5906','exp1-learning','SSW--567','HW-02b' , 'HW-04a']
+        mock_repo_names.return_value = ['csp',' hellogitworld','helloworld','Mocks','Project1','richkempinski.github.io','threads-of-life' ,'try_nbdev','try_nbdev2']
 
         repos = get_repos('richkempinski')
-        self.assertGreaterEqual(len(repos), 4, "User 'richkempinski' has 4 repositories")
+        self.assertGreaterEqual(len(repos), 9, "User 'richkempinski' has 9 repositories")
         self.assertIn('hellogitworld', repos)
         self.assertIn('helloworld', repos)
         self.assertIn('Project1', repos)
         self.assertIn('threads-of-life', repos)
+        self.assertIn('csp',repos)
+        self.assertIn('richkempinskie.github.io',repos)
+        self.assertIn('Mocks',repos)
+        self.assertIn('try_nbdev',repos)
+        self.assertIn('try_nbdev2',repos)
 
-    @mock.patch('githubAPI.retrieve_commits')
-    def mock_retrieve_commits(self, mock_repo_commits):
-        mock_repo_commits.return_value = 2
-        self.assertGreaterEqual(retrieve_commits('apatel5906', 'apatel5906'), 2)
-        mock_repo_commits.return_value = 1
-        self.assertGreaterEqual(retrieve_commits('apatel5906', 'exp1-learning'), 1)
-        mock_repo_commits.return_value = 14
-        self.assertGreaterEqual(retrieve_commits('apatel5906', 'HW-02b'), 14)
-        mock_repo_commits.return_value = 5
-        self.assertGreaterEqual(retrieve_commits('apatel5906', 'HW-04a'), 13)
-        mock_repo_commits.return_value = 19
-        self.assertGreaterEqual(retrieve_commits('apatel5906', 'SSW--567'), 1)
 
-    @mock.patch('githubAPI.get_repos')
+    @mock.patch('GitApi.get_repos')
     def mock_invalid_user(self, mock_repo_name):
         mock_repo_name.return_value = []
         repos = get_repos('sdvnjnWfassvav')
@@ -56,5 +37,4 @@ class MockGitAPI(unittest.TestCase):
 if __name__ == '__main__':
     print("Unit Test: Start")
     unittest.main()
-
 
